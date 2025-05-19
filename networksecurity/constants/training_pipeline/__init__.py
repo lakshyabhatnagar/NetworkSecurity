f72550b60738d0167588604993891d3763f5f4ed
lakshyabhatnagar <lakshyabhatnagar1@gmail.com>
2025-05-19 14:19:52 +0530

    Data Ingestion component completed

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+
+#Definging common constant variables for training pipeline
+TARGET_COLUMN: str= "Result"
+PIPELINE_NAME: str= "NetworkSecurity"
+ARTIFACT_DIR: str= "Artifacts"
+FILE_NAME: str= "phisingData.csv"
+
+TRAIN_FILE_NAME: str= "train.csv"
+TEST_FILE_NAME: str= "test.csv"
+
+
+
+#Data ingestion related constants start with "DATA_INGESTION" var name
+DATA_INGESTION_COLLECTION_NAME: str= "NetworkData"
+DATA_INGESTION_DATABSASE_NAME: str= "network_security"
+DATA_INGESTION_DIR_NAME: str= "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str= "feature_store"
+DATA_INGESTION_INGESTED_DIR: str= "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float= 0.2
